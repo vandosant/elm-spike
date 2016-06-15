5c07c64e3c0bee6a549f040b4f9285d9d62a8e53
vandosant <sjskender@gmail.com>
2016-06-15 12:12:29 -0600

    Create counter-pair

---
+++ counter-pair.elm
@@ -0,0 +1,67 @@
+import Counter
+
+import Html exposing(Html, div, button, text)
+import Html.Events exposing(onClick)
+import Html.App as App
+
+main =
+  App.beginnerProgram
+    { model = init 0 0
+    , update = update
+    , view = view
+    }
+
+
+-- MODEL
+
+
+type alias Model =
+  { topCounter : Counter.Model
+  , bottomCounter : Counter.Model
+  }
+
+
+init : Int -> Int -> Model
+init top bottom =
+  { topCounter = Counter.init top
+  , bottomCounter = Counter.init bottom
+  }
+
+
+
+
+-- UPDATE
+
+
+type Msg
+  = Reset
+  | Top Counter.Msg
+  | Bottom Counter.Msg
+
+
+update : Msg -> Model -> Model
+update message model =
+  case message of
+    Reset ->
+      init 0 0
+
+    Top msg ->
+      { model | topCounter = Counter.update msg model.topCounter }
+
+    Bottom msg ->
+      { model | bottomCounter = Counter.update msg model.bottomCounter }
+
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ App.map Top (Counter.view model.topCounter)
+    , App.map Bottom (Counter.view model.bottomCounter)
+    , button [ onClick Reset ] [ text "RESET" ]
+    ]
+
